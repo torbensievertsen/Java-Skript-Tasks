240e2340fed8ab64f5e779cc3718f0293634f2f3
Torben Sievertsen <torben.sievertsen27@gmail.com>
2023-10-19 08:33:48 +0200

    Finished Task: Use operators & data types

---
+++ Task_Use-operators-and-data-types/script.js
@@ -0,0 +1,18 @@
+console.log("T" + "o" + "r" + "b" + "e" + "n");
+
+// Error! Make it work
+console.log("Hallo");
+// Error! Make it work
+console.log("That doesn't work");
+// should return true. Change only the operator to fix it.
+console.log(50 + 2 >= 52);
+// should be false. Change only the operator to fix it.
+console.log("333" === 333);
+// Wrong result: Expected 555 not 855. Change only the operator to fix it.
+console.log((600 + 510) / 2);
+// Wrong result: Expected a Number 4 received 22
+console.log(2 + 2);
+
+console.log(25 / 5); // Result: 5
+console.log(50 / 10); // Result: 5
+console.log(25 / 5 === 50 / 10);
